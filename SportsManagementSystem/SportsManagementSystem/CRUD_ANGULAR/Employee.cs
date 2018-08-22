a368d5997ce00bc968445d19ee0e8b5443f0f419
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-08-22 11:21:19 +0200

    Add files via upload

---
+++ SportsManagementSystem/SportsManagementSystem/CRUD_ANGULAR/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsManagementSystem.CRUD_ANGULAR
+{
+    public class Employee
+    {
+        public int EmpNo { get; set; }
+        public string EmpName { get; set; }
+        public int Salary { get; set; }
+        public string DeptName { get; set; }
+        public string Designation { get; set; }
+    }
+}
