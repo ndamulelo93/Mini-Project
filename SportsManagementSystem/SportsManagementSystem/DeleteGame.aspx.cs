449f8cbbf81ce3fae6a1ab327852a50fb1d1d3c0
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-08-25 16:16:56 +0200

    Add files via upload

---
+++ SportsManagementSystem/SportsManagementSystem/DeleteGame.aspx.cs
@@ -0,0 +1,37 @@
+﻿using SportClient.ServiceImplementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SportsManagementSystem
+{
+    public partial class DeleteGame : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string gameID = Request.QueryString["G_ID"];
+            int LoggedID = Convert.ToInt32(Session["ID"]);
+            //DeleteGameStatsGameByID
+            //DeleteGameByID
+            //deleteImageByID
+            FileClient flClient = new FileClient();
+            string dl_Image = flClient.deleteGameImageByID(gameID);
+            MatchServiceClient gameClient = new MatchServiceClient();
+            string dl_GameStats = gameClient.DeleteGameStatsGameByID(gameID);
+            string dl_Game = gameClient.DeleteGameByID(gameID);
+            if(dl_Game.ToLower().Contains("success"))
+            {
+                //Popup
+                Response.Redirect("GameManagement.aspx?UserID="+LoggedID);
+            }
+            else
+            {
+                //Popup
+                Response.Redirect("");
+            }
+        }
+    }
+}
