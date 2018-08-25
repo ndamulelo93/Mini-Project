8142d82b98f78eb821e04257d4981ef95016e51b
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-08-25 16:18:17 +0200

    Add files via upload

---
+++ DeleteLeague.aspx.cs
@@ -0,0 +1,31 @@
+﻿using SportClient.ServiceImplementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SportsManagementSystem
+{
+    public partial class DeleteLeague : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string l_ID = Request.QueryString["LeagueID"];
+            int LoggedID = Convert.ToInt32(Session["ID"]);
+            //dl_League
+            LeagueClient lgClient = new LeagueClient();
+            string dl_league = lgClient.dl_League(l_ID);
+            if(dl_league.ToLower().Contains("success"))
+            {
+                //popup
+                Response.Redirect("LeagueList.aspx?UserID=" + LoggedID);
+            }else
+            {
+                //popup
+                Response.Redirect("LeagueList.aspx?UserID=" + LoggedID);
+            }
+        }
+    }
+}
