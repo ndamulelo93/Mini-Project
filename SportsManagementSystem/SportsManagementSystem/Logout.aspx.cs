d60b2e344393ed9093d616ad627c9927ef423e7f
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-08-28 19:20:54 +0200

    Add files via upload
    logout

---
+++ SportsManagementSystem/SportsManagementSystem/Logout.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SportsManagementSystem
+{
+    public partial class Logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Session.Count != 0)
+            {
+                Session.RemoveAll();
+                Response.Redirect("Index.aspx");
+            }else
+            {
+                Response.Redirect("Index.aspx");
+            }
+        }
+    }
+}
