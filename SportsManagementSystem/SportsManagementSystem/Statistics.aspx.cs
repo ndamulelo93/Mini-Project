bd955ebfdccd65493369ebb27108ade950cd94e8
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-08-30 17:20:28 +0200

    Add files via upload
    stats page

---
+++ SportsManagementSystem/SportsManagementSystem/Statistics.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SportsManagementSystem
+{
+    public partial class Statistics : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string strSessionLevel = Convert.ToString(Session["Level"]);
+            if(strSessionLevel.ToLower().Equals("manager"))
+            {
+                ManagerReports.Visible = true;
+                AdminReports.Visible = false;
+            }
+            else if (strSessionLevel.ToLower().Equals("admin"))
+            {
+                ManagerReports.Visible = false;
+                AdminReports.Visible = true;
+            }
+        }
+    }
+}
