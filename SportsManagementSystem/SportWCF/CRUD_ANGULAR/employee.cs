012ea1be5b3fa777a1ae285c46a3544c65b9b785
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-10-02 18:15:24 +0200

    Update employee.cs
    changes on employee file

--- SportsManagementSystem/SportWCF/CRUD_ANGULAR/employee.cs
+++ SportsManagementSystem/SportWCF/CRUD_ANGULAR/employee.cs
@@ -3,3 +3,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+
+namespace SportWCF.CRUD_ANGULAR
+{
+    public class employee
+    {
+        public int EmpNo
+        {
+            get;set;
+        }
+        public string EmpName
+        {
+            get;set;
+        }
+        public int Salary
+        {
+            get;set;
+        }
+        public string DeptName
+        {
+            get;set;
+        }
+        public string Designation
+        {
+            get;set;
+        }
+    }
+}
