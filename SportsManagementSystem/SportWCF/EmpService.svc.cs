0f8098f57195e89d917fc4be4e5df6e5c0594cb4
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-10-02 18:20:32 +0200

    Create EmpService.svc.cs
    lib

---
+++ SportsManagementSystem/SportWCF/EmpService.svc.cs
@@ -0,0 +1,7 @@
+using SportWCF.CRUD_ANGULAR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text
