fcc2160401d8c421b88985b2c83724e796fd27eb
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-11-01 21:26:28 +0200

    Update ISportService.cs

--- SportsManagementSystem/SportWCF/ISportService.cs
+++ SportsManagementSystem/SportWCF/ISportService.cs
@@ -5,3 +5,15 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+
+namespace SportWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ISportService" in both code and config file together.
+    [ServiceContract]
+    public interface ISportService
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
+
