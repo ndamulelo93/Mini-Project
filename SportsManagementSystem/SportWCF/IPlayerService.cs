d9ed98f365630dc4fac9f32698e7ed045c8c16e3
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-11-01 21:18:28 +0200

    Add files via upload

---
+++ SportsManagementSystem/SportWCF/IPlayerService.cs
@@ -0,0 +1,29 @@
+﻿using SportClient.Definition.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SportWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IPlayerService" in both code and config file together.
+    [ServiceContract]
+    public interface IPlayerService
+    {
+        //Get All teams per sport category
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "getAllTeamPlayers/{teamID}", ResponseFormat =
+              WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        List<Player> getAllTeamPlayers(string teamID);
+
+        //Deletions-------------------------------------------------------------->>>>
+        //Delete TTeam Players
+        [OperationContract]
+        [WebInvoke(Method = "DELETE", UriTemplate = "Dl_PlayersBySportID/{ID}", ResponseFormat =
+            WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        string Dl_PlayersBySportID(string ID);
+    }
+}
