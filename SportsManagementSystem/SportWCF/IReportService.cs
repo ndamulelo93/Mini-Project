dede3e44003a973958ee8401e760a2260483981a
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-11-01 21:22:53 +0200

    Update IReportService.cs
    changes

--- SportsManagementSystem/SportWCF/IReportService.cs
+++ SportsManagementSystem/SportWCF/IReportService.cs
@@ -8,3 +8,27 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+
+namespace SportWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IReportService" in both code and config file together.
+    [ServiceContract]
+    public interface IReportService
+    {
+        /// <summary>
+        /// Get game stats
+        /// </summary>
+        /// <param name="Game_ID"></param>
+        /// <param name="type"> type of game stat request</param>
+        /// <returns> list of number of yellow cards, position 0 stores team 1 and position 1 stores team 2'a card</returns>
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "gt_GameStats/{Game_ID},{type}", ResponseFormat =
+            WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        List<int> gt_GameStats(string Game_ID, string type);
+
+
+        //Compare all leagues: Useful for general users to make decisions about which league to follow
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "gt_LeaguesStats", ResponseFormat =
+          WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        List<rep_league> gt_LeaguesStats();
