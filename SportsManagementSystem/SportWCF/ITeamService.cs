0aa06def4eeb0401209d39845c5a99815a214fe1
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-11-01 21:29:42 +0200

    Update ITeamService.cs

--- SportsManagementSystem/SportWCF/ITeamService.cs
+++ SportsManagementSystem/SportWCF/ITeamService.cs
@@ -7,3 +7,20 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+
+namespace SportWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ITeamService" in both code and config file together.
+    [ServiceContract]
+    public interface ITeamService
+    {
+        //Insert
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "AddTeam", ResponseFormat =
+          WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        int AddTeam(Team _team);
+        //Edit
+        [OperationContract]
+        [WebInvoke(Method = "PUT", UriTemplate = "EditTeam", ResponseFormat =
+          WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        string EditTeam(Team _team);
