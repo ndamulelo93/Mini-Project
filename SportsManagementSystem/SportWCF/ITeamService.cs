5cdaaba6a5da71fca7feb328dc1e2efca493997d
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-11-01 21:29:20 +0200

    Update ITeamService.cs
    libraries

--- SportsManagementSystem/SportWCF/ITeamService.cs
+++ SportsManagementSystem/SportWCF/ITeamService.cs
@@ -1,1 +1,9 @@
 
+using SportClient.Definition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
