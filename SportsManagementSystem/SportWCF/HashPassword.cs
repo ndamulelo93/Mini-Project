9e9cc36c41198ee2a58bccd45fb424c922ba7782
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-11-01 21:07:09 +0200

    Add files via upload

---
+++ SportsManagementSystem/SportWCF/HashPassword.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace SportWCF
+{
+    public class HashPassword
+    {
+        //  SHA1 algorithm;
+        public static string HashPass(string password)
+        {
+            SHA1 algorithm = SHA1.Create();
+            byte[] byteArray = null;
+            byteArray = algorithm.ComputeHash(Encoding.Default.GetBytes(password));
+            string hashedPassword = "";
+            for (int i = 0; i < byteArray.Length; i++)
+            {
+                hashedPassword += byteArray[i].ToString("x2");
+            }
+            return hashedPassword;
+        }
+    }
+}
