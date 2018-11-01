7dd320de6c911243e12b73c4437e89e5a886223a
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-11-01 21:18:46 +0200

    Add files via upload

---
+++ SportsManagementSystem/SportWCF/IRegistrationService.cs
@@ -0,0 +1,28 @@
+﻿using SportClient.Definition.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SportWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRegistrationService" in both code and config file together.
+    [ServiceContract]
+    public interface IRegistrationService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "registerUser", ResponseFormat =
+            WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        int registerUser(BASE_USER user);
+
+        //Individual Player
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "registerPlayer", ResponseFormat =
+             WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        string registerPlayer(Player player);
+
+    }
+}
