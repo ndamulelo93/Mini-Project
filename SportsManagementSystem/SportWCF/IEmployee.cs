5bacf3aa01147cf98a8968dd83f5988304b24d09
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-11-01 21:07:39 +0200

    Add files via upload

---
+++ SportsManagementSystem/SportWCF/IEmployee.cs
@@ -0,0 +1,18 @@
+﻿using SportWCF.CRUD_ANGULAR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SportWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IEmployee" in both code and config file together.
+    [ServiceContract]
+    public interface IEmployee
+    {
+
+    }
+}
