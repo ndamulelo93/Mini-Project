2512b7cbbde67d40d1c7c09202b658f807219c29
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-11-01 21:17:11 +0200

    Add files via upload

---
+++ SportsManagementSystem/SportWCF/ILoginService.cs
@@ -0,0 +1,27 @@
+﻿using SportClient.Definition.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SportWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ILoginService" in both code and config file together.
+    [ServiceContract]
+    public interface ILoginService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "Login/{email},{password}", ResponseFormat =
+             WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        BASE_USER Login(string email, string password);
+
+        //Edit User
+        [OperationContract]
+        [WebInvoke(Method = "PUT", UriTemplate = "UpdateUser", ResponseFormat =
+          WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        string UpdateUser(BASE_USER user);
+    }
+}
