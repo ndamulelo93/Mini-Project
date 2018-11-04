cefa4c8453d0453d00f03f9792de4bb8a1a54769
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-11-04 13:47:39 +0200

    Update RegistrationService.svc.cs

--- SportsManagementSystem/SportWCF/RegistrationService.svc.cs
+++ SportsManagementSystem/SportWCF/RegistrationService.svc.cs
@@ -5,3 +5,43 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 using SportClient.Definition.Users;
+
+namespace SportWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RegistrationService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RegistrationService.svc or RegistrationService.svc.cs at the Solution Explorer and start debugging.
+    public class RegistrationService : IRegistrationService
+    {
+        public string registerPlayer(Player player)
+        {
+
+            
+            using (SPORT_LINK_DBDataContext db = new SPORT_LINK_DBDataContext())
+            {
+                try
+                {
+                    int isAvail = (from eh in db.TEAMPLAYERs where eh.Name.Equals(player.Name) &&
+                                   eh.Sport_Id.Equals(player.SportID) select eh).Count();
+                    if (isAvail == 0)
+                    {
+                        TEAMPLAYER _player = new TEAMPLAYER();
+                        _player.Name = player.Name;
+                        _player.Position = player.Position;
+                        _player.PerformanceRate = player.PerformanceRate;
+                        _player.Description = player.Desc;
+                        _player.Sport_Id = player.SportID;
+                        db.TEAMPLAYERs.InsertOnSubmit(_player);
+                        db.SubmitChanges();
+                        return "Registered " + _player.Name + " successfully";
+                    }
+                    else
+                    {
+                        return "Error: Account already taken";
+                    }
+                }
+                catch (Exception e)
+                {
+                    return e.GetBaseException().ToString();
+                }
+            };
+        }
