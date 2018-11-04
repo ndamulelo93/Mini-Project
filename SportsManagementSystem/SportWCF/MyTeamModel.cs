0156e15d9e647726c5d085b1210a674fe60bcc55
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-11-04 13:41:33 +0200

    Update MyTeamModel.cs

--- SportsManagementSystem/SportWCF/MyTeamModel.cs
+++ SportsManagementSystem/SportWCF/MyTeamModel.cs
@@ -1,1 +1,47 @@
 
+using SportClient.Definition.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportWCF
+{
+    public class MyTeamModel
+    {
+        public int ID
+        {
+            get;set;
+        }
+        public string Type
+        {
+            get;set;
+        }
+        public string Name
+        {
+            get;set;
+        }
+        public int NumPlayers
+        {
+            get;set;
+        }
+        public string Desc
+        {
+            get;set;
+        }
+        public int foreignID
+        {
+            get;set;
+        }
+        public string ManagerName
+        {
+            get;set;
+        }
+        public string ImagePath
+        {
+            get;set;
+        }
+
+        
+    }
+}
