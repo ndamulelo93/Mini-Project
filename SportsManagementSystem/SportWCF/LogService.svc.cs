7e1e902d1a412e956574b6c3b3ec567e34dafb4d
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-11-04 13:36:51 +0200

    Update LogService.svc.cs
    some changes in log service

--- SportsManagementSystem/SportWCF/LogService.svc.cs
+++ SportsManagementSystem/SportWCF/LogService.svc.cs
@@ -6,3 +6,41 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 using SportClient.Definition;
+
+namespace SportWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "LogService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select LogService.svc or LogService.svc.cs at the Solution Explorer and start debugging.
+    public class LogService : ILogService
+    {
+        public string CreateLog(Log log)
+        {
+            try
+            {
+                using (SPORT_LINK_DBDataContext db = new SPORT_LINK_DBDataContext())
+                {
+                    var query = (from res in db.LOGs where res.Log_Id.Equals(log.ID) && res.League_Id.Equals(log.League_ID) select res);
+                    if (query.Count() == 0) //AddTeam
+                    {
+                        LOG toinsert = new LOG();
+                        toinsert.TeamName = log.TeamName;
+                        toinsert.Position = log.Position;
+                        toinsert.MatchPlayed = log.MatchPlayed;
+                        toinsert.Wins = log.Wins;
+                        toinsert.Draws = log.Draws;
+                        toinsert.Points = log.Points;
+                        toinsert.League_Id = log.League_ID;
+                        db.LOGs.InsertOnSubmit(toinsert);
+                        db.SubmitChanges();
+                        return "success";
+                    }else
+                    {
+                        return "failed: log already exist";
+                    }
+                };
+            }
+            catch (Exception)
+            {
+                return "failed: catched";
+            }
+        }
