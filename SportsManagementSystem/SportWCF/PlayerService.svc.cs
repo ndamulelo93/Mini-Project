8be9b03d0032a9eaa501bb4049b83f9f386de486
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-11-04 13:45:44 +0200

    Update PlayerService.svc.cs
    player service

--- SportsManagementSystem/SportWCF/PlayerService.svc.cs
+++ SportsManagementSystem/SportWCF/PlayerService.svc.cs
@@ -6,3 +6,34 @@ using System.ServiceModel;
 using System.Text;
 using SportClient.Definition.Users;
 
+namespace SportWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "PlayerService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select PlayerService.svc or PlayerService.svc.cs at the Solution Explorer and start debugging.
+    public class PlayerService : IPlayerService
+    {
+        public string Dl_PlayersBySportID(string ID)
+        {
+            int _id = Convert.ToInt32(ID);
+            try
+            {
+                using (SPORT_LINK_DBDataContext db = new SPORT_LINK_DBDataContext())
+                {
+                    List<TEAMPLAYER> toDelete = (from dl in db.TEAMPLAYERs where dl.Sport_Id == _id select dl).ToList();
+                    if (toDelete == null)
+                    {
+                        return "Failed: Ticket Not Found";
+                    }
+                    else
+                    {
+                        db.TEAMPLAYERs.DeleteAllOnSubmit(toDelete);
+                        db.SubmitChanges();
+                        return "success";
+                    }
+                };
+            }
+            catch (Exception)
+            {
+                return "Failed";
+            }
+        }
