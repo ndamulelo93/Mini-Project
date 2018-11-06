e9adef67d4503faa5acdf1bab1dfd2fb3b91b3fd
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-11-06 21:25:06 +0200

    Update SportService.svc.cs
    sport service

--- SportsManagementSystem/SportWCF/SportService.svc.cs
+++ SportsManagementSystem/SportWCF/SportService.svc.cs
@@ -1,1 +1,19 @@
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SportWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "SportService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select SportService.svc or SportService.svc.cs at the Solution Explorer and start debugging.
+    public class SportService : ISportService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
