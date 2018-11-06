d174d50f14e1e28169fd88f23a9cac8e30555ef0
ndamulelo93 <32490881+ndamulelo93@users.noreply.github.com>
2018-11-06 21:26:58 +0200

    Update TeamModel.cs
    sport model

--- SportsManagementSystem/SportWCF/TeamModel.cs
+++ SportsManagementSystem/SportWCF/TeamModel.cs
@@ -1,1 +1,70 @@
 
+using SportClient.Definition.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportClient.Definition
+{
+    public class TeamModel
+    {
+       public int G_ID
+        {
+            get;set;
+        }
+        public string TeamOne
+        {
+            get;set;
+        }
+        public string TeamTwo
+        {
+            get; set;
+        }
+        public string Date
+        {
+            get; set;
+        }
+        public string Venue
+        {
+            get; set;
+        }
+        public string Type
+        {
+            get; set;
+        }
+        public int  LeagueID
+        {
+            get; set;
+        }
+        public string ImagePath
+        {
+            get; set;
+        }
+
+        public string LeaguName
+        {
+            get; set;
+        }
+        public int s_ID
+        {
+            get;set;
+        }
+        public string TeamName
+        {
+            get;set;
+        }
+        public int NumPlayers
+        {
+            get;set;
+        }
+        public string Desc
+        {
+            get;set;
+        }
+        public List<Player> players
+        {
+            get;set;
+        }
+    }
+}
